09e550af88262e18b18ee08a945d3c433bd6c0ea
Emerson Rogerio Alves dos Santos <erogerio@lnxcit015672.cit>
2020-03-01 15:25:03 -0300

    Updates

---
+++ precache-manifest.3443f7b54dbdd6acbed09b82eaaf50ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186a673d00c0e3aa12bc9b60cfde19c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df75cbffe30276e93e55",
+    "url": "/static/css/main.7ab697ce.chunk.css"
+  },
+  {
+    "revision": "fbe3a98473d4de8fa8cf",
+    "url": "/static/js/2.f1be4715.chunk.js"
+  },
+  {
+    "revision": "f3e237cf2cae75eaca7b3f666862bf66",
+    "url": "/static/js/2.f1be4715.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df75cbffe30276e93e55",
+    "url": "/static/js/main.63a3058c.chunk.js"
+  },
+  {
+    "revision": "cc1bfb78501f5c6a2551",
+    "url": "/static/js/runtime-main.26e0d32a.js"
+  },
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/static/media/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/static/media/Poppins-Regular.731a28a4.ttf"
+  }
+]);
