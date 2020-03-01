19836e420dad4fc351fddf623da9ff6e01ff3b15
Emerson Rogerio Alves dos Santos <erogerio@lnxcit015672.cit>
2020-03-01 16:01:45 -0300

    Updates

---
+++ precache-manifest.86c5b1fc8a449ac655611fbf6dae6ecf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe877d5d78f4dbd5a852e621e267bcd2",
+    "url": "/cvm-web/index.html"
+  },
+  {
+    "revision": "9850e4e954873f33afdd",
+    "url": "/cvm-web/static/css/main.95d59c39.chunk.css"
+  },
+  {
+    "revision": "fbe3a98473d4de8fa8cf",
+    "url": "/cvm-web/static/js/2.f1be4715.chunk.js"
+  },
+  {
+    "revision": "f3e237cf2cae75eaca7b3f666862bf66",
+    "url": "/cvm-web/static/js/2.f1be4715.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9850e4e954873f33afdd",
+    "url": "/cvm-web/static/js/main.66367314.chunk.js"
+  },
+  {
+    "revision": "8e6fe58c566bacbd4c06",
+    "url": "/cvm-web/static/js/runtime-main.6b1855da.js"
+  },
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/cvm-web/static/media/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/cvm-web/static/media/Poppins-Regular.731a28a4.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3443f7b54dbdd6acbed09b82eaaf50ef.js"
+  "/cvm-web/precache-manifest.86c5b1fc8a449ac655611fbf6dae6ecf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cvm-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
