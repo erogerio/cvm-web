79fe6145d4f036c7645a453bbdcae2bb19ac512f
Emerson Rogerio Alves dos Santos <erogerio@lnxcit015672.cit>
2020-03-01 12:50:18 -0300

    Updates

---
+++ precache-manifest.421a3539b9f3adbcf4f5b7b36dccfba4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e469af73f1b121efcd55ee338312095",
+    "url": "/cvm/index.html"
+  },
+  {
+    "revision": "dc55054349b8bdc5bc3b",
+    "url": "/cvm/static/css/main.3acaaf23.chunk.css"
+  },
+  {
+    "revision": "fbe3a98473d4de8fa8cf",
+    "url": "/cvm/static/js/2.f1be4715.chunk.js"
+  },
+  {
+    "revision": "f3e237cf2cae75eaca7b3f666862bf66",
+    "url": "/cvm/static/js/2.f1be4715.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc55054349b8bdc5bc3b",
+    "url": "/cvm/static/js/main.97feca5e.chunk.js"
+  },
+  {
+    "revision": "b7c257478b04d4f50225",
+    "url": "/cvm/static/js/runtime-main.a3d8e2e1.js"
+  },
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/cvm/static/media/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/cvm/static/media/Poppins-Regular.731a28a4.ttf"
+  }
+]);
